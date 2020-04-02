f19fb5826b632bc6cfdafbdb66e055fde9e205e3
DannyPX <Damnoga@hotmail.com>
2020-04-02 12:01:44 +0200

    package start script

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -4,7 +4,7 @@ const state = { weather: [] }
 
 const mutations = { 'SET_WEATHER' (state, weather) { state.weather = weather } }
 
-const actions = { getWeather: ({ commit }) => { axios.get('https://tatu-sm.herokuapp.com/api/9292/getTrips/station-eindhoven&station-helmond&2020-04-04T1754').then(response => { commit('SET_WEATHER', response.data) }) } }
+const actions = { getWeather: ({ commit }, lat, lon) => { axios.get(`https://https://tatu-sm.herokuapp.com/api/weer/get2HForecast/${lat}&${lon}`).then(response => { commit('SET_WEATHER', response.data) }) } }
 
 const getters = {
 }
